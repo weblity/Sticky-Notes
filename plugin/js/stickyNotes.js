a7fd5bf21f50b98a2d76c8153b94d7045c264305
Manas Kumar Pradhan <45661927+weblity@users.noreply.github.com>
2018-12-07 16:18:51 +0100

    Added Sticky To-Do Notes made with vue.js Plugin
    Added Sticky To-Do Notes made with vue.js Plugin

---
+++ plugin/js/stickyNotes.js
@@ -0,0 +1,48 @@
+// Use Strict Mode
+"use strict";
+
+(function stickyNotes() {
+
+    /*
+    ---------------------------
+        Functions
+        Last change:    07/12/2018
+        Assigned to:    Weblity.com
+    ---------------------------
+    */
+
+    // Define a function to get link parameter
+    function getParam(query) {
+        var param = {};
+        var link = window.location.search;
+        link = link.replace('?', '');
+        var divide = link.split('&').forEach(function(variable){
+           var half = variable.split('=');
+           param[half[0]] = half[1];
+        });
+        
+        if (param[query]) {
+            return param[query];
+        } else {
+            return '';
+        }
+    }
+
+    // Vue Instances
+    var header = new Vue({
+        el: '#header',
+        data: {
+            isHashtag: getParam('s')
+        }
+    });
+
+
+    var addNote = new Vue({
+        el: '#notes',
+        components: {
+            'sticky-notes': httpVueLoader('components/multiNotes.vue'),
+            'shared-note': httpVueLoader('components/sharedNote.vue')
+        }
+    });    
+
+})();
